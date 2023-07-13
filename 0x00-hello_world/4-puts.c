25ad45d34c55e85af1b081aba49e70d6cb26b6f9
menna <m94menna@gmail.com>
2023-07-13 05:28:47 -0700

    c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include<studio.h>
+#include <stdio.h>
 /**
- * main 
- * Return 0
- *
+ * void  main function - Prints a string
+ * Return: 0 if success
  */
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
