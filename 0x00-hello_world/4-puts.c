8df5fc3afa697b32bca74c2df5779dec5276c69b
menna <m94menna@gmail.com>
2023-07-13 05:34:00 -0700

    c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * Description: void  main function - Prints a string
+ * Description: main - function - Prints a string
  * Return: 0 if success
+ *
  */
 int main(void)
 {
