0739748edad1bfb20132001b26e4a3208f0e50d1
menna <m94menna@gmail.com>
2023-07-18 09:35:14 -0700

    1st putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <unistd.h>
+
+/**
+ * _putchar - print char c
+ * @c: The character to print
+ *
+ * Return 1
+ * otherwise -1 returned
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
