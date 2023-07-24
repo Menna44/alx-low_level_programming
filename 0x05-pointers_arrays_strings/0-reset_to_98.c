cc8a4145f1e93929113fdacf93b42625b56d317f
menna <m94menna@gmail.com>
2023-07-24 08:37:37 -0700

    1st q

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to take a pointer
+ * updates the value it points to to 98.
+ * @n: This is the entry
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
